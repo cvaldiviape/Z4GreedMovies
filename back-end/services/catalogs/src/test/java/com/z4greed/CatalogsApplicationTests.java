bce4542dbae9d4c8f9031a69b64cdd15b976300d
cvaldiviape <cvaldiviape@gmail.com>
2023-08-20 21:38:28 -0500

    buid: add base services

---
+++ back-end/services/catalogs/src/test/java/com/z4greed/CatalogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.z4greed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ back-end/services/finances/src/test/java/com/z4greed/FinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.z4greed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
