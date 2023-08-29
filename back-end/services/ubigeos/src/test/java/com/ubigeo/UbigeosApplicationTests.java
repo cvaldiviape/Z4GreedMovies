859a2348e517982d295028e024e95489b5a7d682
cvaldiviape <cvaldiviape@gmail.com>
2023-08-28 22:54:07 -0500

    build: add service ubigeo

---
+++ back-end/services/ubigeos/src/test/java/com/ubigeo/UbigeosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubigeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UbigeosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
