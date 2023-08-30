f4a3aef5cdfd1a6850af1fd3154cabc5ab038ad4
cvaldiviape <cvaldiviape@gmail.com>
2023-08-30 12:41:52 -0500

    build (backend): connect service catalog with eureka server

--- back-end/services/catalogs/src/main/java/com/z4greed/CatalogsApplication.java
+++ back-end/services/catalogs/src/main/java/com/z4greed/CatalogsApplication.java
@@ -2,7 +2,13 @@ package com.z4greed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+// "@EnableEurekaClient": habilita la funcionalidad de descubrimiento de servicios de Eureka. Eureka es un servicio de descubrimiento de servicios que permite a los servicios
+// de la aplicación registrarse en él y descubrir y comunicarse con otros servicios que están registrados.
+@EnableEurekaClient
+@EnableFeignClients // habilitando todos los clientes Feing de mi proyecto, además de permitir la inyección de ese cliente.
 @SpringBootApplication
 public class CatalogsApplication {
 
