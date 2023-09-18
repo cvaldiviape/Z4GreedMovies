5e55018e369bb37d7f2d3ad467eec27e67d496a7
cvaldiviape <cvaldiviape@gmail.com>
2023-09-17 21:36:22 -0500

    feat (backend): add new message in GeneralErrorEnum - service shared

--- back-end/services/shared/src/main/java/com/shared/error/GeneralErrorEnum.java
+++ back-end/services/shared/src/main/java/com/shared/error/GeneralErrorEnum.java
@@ -6,7 +6,8 @@ public enum GeneralErrorEnum  implements GenericError {
     ER000001("ER000001", "%s es requerido!", WARNING),
     ER000002("ER000002", "%s no existe!", WARNING),
     ER000003("ER000003", "%s no contiene elementos!", WARNING),
-    ER000004("ER000004", "%s con ID: %s no existe!", WARNING);
+    ER000004("ER000004", "%s con ID: %s no existe!", WARNING),
+    ER000005("ER000005", "Ya existe un registro con este %s: %s", WARNING);
 
     private GeneralErrorEnum(String code, String message, TypeExceptionEnum type) {
         this.code = code;
