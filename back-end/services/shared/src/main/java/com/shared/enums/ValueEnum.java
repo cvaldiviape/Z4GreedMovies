a0dcb01939aa9bc348bcb1a17cf25e3d83d43cbb
cvaldiviape <cvaldiviape@gmail.com>
2023-09-17 22:31:09 -0500

    feat (backend): add more values in ValueEnum - service shared

--- back-end/services/shared/src/main/java/com/shared/enums/ValueEnum.java
+++ back-end/services/shared/src/main/java/com/shared/enums/ValueEnum.java
@@ -7,8 +7,16 @@ public enum ValueEnum {
     COUNTRY("Pais"),
     GENRE("Gémero"),
     LANGUAGE("Lenguaje"),
-    AUDIENCE("Audencia"),
-    TYPE_STATUS_FILM_SHOW("Tipo de pelicula");
+    AUDIENCE("Tipo de audiencia"),
+    TYPE_STATUS_FILM_SHOW("Tipo de pelicula"),
+    PRODUCT("Producto"),
+    LIST_CATEGORY("Lista de categorias"),
+    LIST_COUNTRY("Lista de paises"),
+    LIST_GENRE("Lista de géneros"),
+    LIST_LANGUAGE("Lista de lenguajes"),
+    LIST_AUDIENCE("Lista de tipos de audiencias"),
+    LIST_TYPE_STATUS_FILM_SHOW("Lista de tipos de peliculas"),
+    LIST_PRODUCT("Lista de productos");
 
     private final String value;
 
