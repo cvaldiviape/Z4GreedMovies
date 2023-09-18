96065de735a19854d21b6c10e09266eeb7e68ae6
cvaldiviape <cvaldiviape@gmail.com>
2023-09-17 22:05:45 -0500

    refactor (backend): refactor ValueEnum - service shared

--- back-end/services/shared/src/main/java/com/shared/enums/ValueEnum.java
+++ back-end/services/shared/src/main/java/com/shared/enums/ValueEnum.java
@@ -1,2 +1,28 @@
-package com.shared.enums;public enum ValueEnum {
+package com.shared.enums;
+
+public enum ValueEnum {
+    NAME("nombre"),
+    CODE("código"),
+    CATEGORY("Categoria"),
+    COUNTRY("Pais"),
+    GENRE("Gémero"),
+    LANGUAGE("Lenguaje"),
+    AUDIENCE("Audencia"),
+    TYPE_STATUS_FILM_SHOW("Tipo de pelicula");
+
+    private final String value;
+
+    private ValueEnum(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
 }
