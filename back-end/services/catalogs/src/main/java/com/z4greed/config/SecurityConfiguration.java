13b038b0dec1cba3c63ac17c2d7a7edd4308113d
cvaldiviape <cvaldiviape@gmail.com>
2023-09-24 23:32:10 -0500

    feat (backend): configure route protection for direct requests - service catalog

--- back-end/services/catalogs/src/main/java/com/z4greed/config/SecurityConfiguration.java
+++ back-end/services/catalogs/src/main/java/com/z4greed/config/SecurityConfiguration.java
@@ -1,67 +1,36 @@
-//package com.z4greed.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-//import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.oauth2.jwt.JwtDecoder;
-//import org.springframework.security.oauth2.jwt.JwtDecoders;
-//import org.springframework.security.web.SecurityFilterChain;
+package com.z4greed.config;
 
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfiguration {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.web.SecurityFilterChain;
 
-//    @Override
-//    public void configure(HttpSecurity httpSecurrity) throws Exception {
-//        httpSecurrity.authorizeRequests(authorize -> authorize.anyRequest().authenticated())
-//                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt); // con esto garantizo que cualquier request que venga a "service catalogs" sera interceptado por este metodo "public void configure()"
-//                                                                            // y sera validado por el mercanismo "OAuth2ResourceServer".
-//    }
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-//        return httpSecurity
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(auth -> auth
-////                        .requestMatchers("/token/**").permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-////                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
-//                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
-////                .httpBasic(Customizer.withDefaults())
-//                .build();
-//
-////        httpSecurity.oauth2ResourceServer()
-////                .jwt();
-////
-////        httpSecurity.sessionManagement()
-////                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//
-////        return httpSecurity.build();
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(jwt -> {}))
+                .build();
+    }
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-//        return httpSecurity
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .oauth2ResourceServer((oauth2) -> oauth2.jwt(jwt -> {}))
-//                .build();
-//    }
-//
-//    @Bean
-//    public JwtDecoder jwtDecoder() {
-//        return JwtDecoders.fromIssuerLocation("http://localhost:9090/realms/z4movies");
-//    }
+    // Configurando el decodificador de tokens JWT para que valide tokens JWT emitidos por el servidor de autorización (Keycloak en este caso) que tiene la
+    // ubicación especificada como emisor (issuer). En este caso, la URL del emisor es "http://localhost:9090/realms/z4movies-dev".
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return JwtDecoders.fromIssuerLocation("http://localhost:9090/realms/z4movies-dev");
+    }
 
     // documentacion: https://docs.spring.io/spring-security/reference/servlet/oauth2/resource-server/jwt.html
-
-//}
+}
