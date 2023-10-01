80534587628ea2f265b761c249479bfa601ce76f
cvaldiviape <cvaldiviape@gmail.com>
2023-09-30 22:47:58 -0500

    remove (backend): delete incepetors and handler exception - service catalog

--- back-end/services/catalogs/src/main/java/com/z4greed/CatalogsApplication.java
+++ back-end/services/catalogs/src/main/java/com/z4greed/CatalogsApplication.java
@@ -3,8 +3,10 @@ package com.z4greed;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 // "@EnableDiscoveryClient": habilita la funcionalidad de descubrimiento de servicios de Eureka. Eureka es un servicio de descubrimiento de servicios que permite a los servicios
 // de la aplicación registrarse en él y descubrir y comunicarse con otros servicios que están registrados.
@@ -12,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients // habilitando todos los clientes Feing de mi proyecto, además de permitir la inyección de ese cliente.
 @OpenAPIDefinition
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.z4greed", "com.shared.interceptors", "com.shared.exception"})
 public class CatalogsApplication {
 
 	public static void main(String[] args) {
