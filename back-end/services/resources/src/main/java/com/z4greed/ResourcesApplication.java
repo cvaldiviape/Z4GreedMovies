f6f39df4af9de736ec2c49426cbf0fb324348a6d
cvaldiviape <cvaldiviape@gmail.com>
2023-09-30 22:48:09 -0500

    remove (backend): delete incepetors and handler exception - service resource

--- back-end/services/resources/src/main/java/com/z4greed/ResourcesApplication.java
+++ back-end/services/resources/src/main/java/com/z4greed/ResourcesApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
 @EnableFeignClients
 @OpenAPIDefinition
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.z4greed", "com.shared.interceptors", "com.shared.exception"})
 public class ResourcesApplication {
 
 	public static void main(String[] args) {
