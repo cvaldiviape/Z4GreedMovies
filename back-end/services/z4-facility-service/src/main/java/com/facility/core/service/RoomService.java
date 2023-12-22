ad0935b7419ad41bd10efacb2dec975ea329cd8d
cvaldiviape <cvaldiviape@gmail.com>
2023-12-21 23:58:45 -0500

    remove (backend): delete files unused - service facility

--- back-end/services/z4-facility-service/src/main/java/com/facility/core/service/RoomService.java
+++
@@ -1,8 +0,0 @@
-package com.facility.core.service;
-
-import com.shared.dto.external.facility.RoomDto;
-import com.facility.core.service.common.CrudService;
-
-public interface RoomService extends CrudService<RoomDto, Integer> {
-
-}
